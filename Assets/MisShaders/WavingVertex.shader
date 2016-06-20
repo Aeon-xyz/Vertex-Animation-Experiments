413398e35e078966bacfe2c0b643fe3ef61cb0f4
Aeon-xyz <enum.script@gmail.com>
2016-06-20 16:20:13 -0500

    First Commit
    First

---
+++ Assets/MisShaders/WavingVertex.shader
@@ -0,0 +1,53 @@
+﻿Shader "AeonXYZ/WavingVertex" {
+	Properties
+	{
+		_Value1("Value 1", Float) = 0
+		_MainTex("Texture", 2D) = "white" { }
+	}
+
+		SubShader
+	{
+		Pass
+	{
+		CULL Off
+
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.cginc"
+#include "AutoLight.cginc"
+
+	sampler2D _MainTex;
+	uniform float _Value1;
+	
+
+	struct appdata {
+		float4 vertex : POSITION;
+		float4 texcoord : TEXCOORD0;
+	};
+
+	struct v2f {
+		float4 pos : POSITION;
+		float2 uv: TEXCOORD0;
+	};
+
+	v2f vert(appdata_base v) {
+		v2f o;
+		v.vertex.xyz += sin((v.vertex.y + _Time *10)*3.16)*_Value1;
+		o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+		o.uv = v.texcoord;
+		return o;
+	}
+
+	float4 frag(v2f i) : COLOR
+	{
+		half4 color = tex2D(_MainTex, i.uv);
+		return color;
+	}
+
+		ENDCG
+
+	}
+	}
+		Fallback "VertexLit"
+}
---
+++ Assets/MisUtilidades/ChangeStatue.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeStatue : MonoBehaviour {
+
+    public GameObject miEstatua1;
+    public GameObject miEstatua2;
+    public GameObject miEstatua3;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            miEstatua1.SetActive(true);
+            miEstatua2.SetActive(false);
+            miEstatua3.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            miEstatua1.SetActive(false);
+            miEstatua2.SetActive(true);
+            miEstatua3.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            miEstatua1.SetActive(false);
+            miEstatua2.SetActive(false);
+            miEstatua3.SetActive(true);
+        }
+    }
+}
